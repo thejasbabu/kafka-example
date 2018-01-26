93cacf882bca89c9d2ef47ffe1553af575118d66
Thejas <thejasb@thoughtworks.com>
2018-01-26 18:48:47 +0530

    Implement Consumer and Runner

---
+++ src/main/java/kafkaExample/Consumer.java
@@ -0,0 +1,32 @@
+package kafkaexample;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
+        KafkaConsumer consumer = new KafkaConsumer(props);
+        consumer.subscribe(Arrays.asList("odd", "even"));
+        int counter = 0;
+        while (counter <= 1000) {
+            ConsumerRecords<String, String> recs = consumer.poll(100);
+            if (recs.count() == 0) {
+            } else {
+                for (ConsumerRecord<String, String> rec : recs) {
+                    System.out.printf("%s: %s", rec.key(), rec.value());
+                }
+            }
+            counter++;
+        }
+    }
+}
---
+++ src/main/java/kafkaExample/Runner.java
@@ -0,0 +1,13 @@
+package kafkaexample;
+
+public class Runner {
+    public static void main(String[] args) throws InterruptedException {
+        if (args[0].equals("producer")) {
+            Producer.main(args);
+        } else if (args[0].equals("consumer")) {
+            Consumer.main(args);
+        } else {
+            throw new IllegalArgumentException("Don't know how to do " + args[0]);
+        }
+    }
+}
